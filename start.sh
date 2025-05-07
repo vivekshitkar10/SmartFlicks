b7a8720754714ba8984b15283cde0df765c2c080
Vivek Shitkar <92878509+vivekshitkar10@users.noreply.github.com>
2025-05-07 17:09:53 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# Log the download process
+echo "Starting the download of similarity.pkl"
+gdown https://drive.google.com/uc?id=1xLmlmFLb4Do-R77Twyk-tt4o3LAru7YJ -O similarity.pkl
+echo "Download completed."
 
-# Check if similarity.pickle is already downloaded
-if [ ! -f "similarity.pickle" ]; then
-  echo "Downloading similarity.pickle from Google Drive..."
-  gdown https://drive.google.com/uc?id=1xLmlmFLb4Do-R77Twyk-tt4o3LAru7YJ -O similarity.pickle
+# Check if the file exists
+if [ -f similarity.pkl ]; then
+    echo "similarity.pkl file downloaded successfully."
+else
+    echo "similarity.pkl download failed."
+    exit 1
 fi
 
 # Start the Streamlit app
